03511eb6b1850432cc687c9c9e2771216c9d75a4
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-07-07 17:08:32 +0100

    update workflows to new paths

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,5 +3,5 @@ npm install
 npx puppeteer browsers install chrome
 WORKING_DIR=$(pwd)
 cp -r $WORKING_DIR/comparison_interface/static/example.images/ $WORKING_DIR/comparison_interface/static/images/
-cp $WORKING_DIR/comparison_interface/configuration/example.flask.py $WORKING_DIR/comparison_interface/configuration/flask.py
+cp $WORKING_DIR/comparison_interface/main/configuration/example.flask.py $WORKING_DIR/comparison_interface/main/configuration/flask.py
 
