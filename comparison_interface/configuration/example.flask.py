af17fe11a479d4036d8b328c1843f1bb7bfb7081
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-08-07 17:03:03 +0100

    change the example file - needs more changes

--- comparison_interface/configuration/example.flask.py
+++ comparison_interface/configuration/example.flask.py
@@ -2,9 +2,30 @@ class Settings(object):
     """Flask configuration."""
 
     SECRET_KEY = 'srd6sj5sjfMS12HD'  # Change to your own secret key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///admin_database.db'
+    SQLALCHEMY_BINDS = {
+        'study_db': 'sqlite:///database.db',
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_MINUTES_VALIDITY = 240  # Session expires after 4 hours of inactivity
     API_ACCESS = False
     API_KEY_FILE = '.apikey'
     LANGUAGE = 'en'
+    IMAGE_UPLOAD_DIR = 'static/images/'
+    HTML_PAGES_DIR = 'pages_html'
+    CONFIG_UPLOAD_DIR = 'project_configuration'
+    MAIL_BACKEND = 'console'
+    #MAIL_SERVER = 'our path'
+    #MAIL_PORT = 25
+    SECURITY_PASSWORD_SALT = 'ttr5redsijokj'
+    SECURITY_URL_PREFIX = '/admin'
+    SECURITY_LOGIN_USER_TEMPLATE = 'user_login.html'
+    SECURITY_TWO_FACTOR = True
+    SECURITY_TWO_FACTOR_REQUIRED = True
+    SECURITY_TWO_FACTOR_ENABLED_METHODS = ['email']
+    SECURITY_TWO_FACTOR_VERIFY_CODE_TEMPLATE = '2fa_verify.html'
+    SECURITY_TWO_FACTOR_SETUP_TEMPLATE = '2fa_setup.html'
+    SECURITY_TOTP_SECRETS = {"1": "retfdgdgfdsfk"}
+    SECURITY_TOTP_ISSUER = 'flask admin'
+    SECURITY_POST_LOGIN_VIEW = '/admin/dashboard'
+    SECURITY_POST_LOGOUT_VIEW = '/admin/logged-out'
