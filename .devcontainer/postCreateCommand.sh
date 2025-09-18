cc6adfbf03803f0021c88abbb6213f0c37acae1d
Cat Smith <910384+catsmith@users.noreply.github.com>
2025-09-18 15:42:46 +0100

    fix the copying

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,7 @@ pip3 install -e ".[admin,lint,test]"
 npm install
 npx puppeteer browsers install chrome
 WORKING_DIR=$(pwd)
-cp -r $WORKING_DIR/comparison_interface/static/example.images/ $WORKING_DIR/comparison_interface/static/images/
-cp -r $WORKING_DIR/comparison_interface/example.pages_html/ $WORKING_DIR/comparison_interface/pages_html/
+cp -TR $WORKING_DIR/comparison_interface/static/example.images/ $WORKING_DIR/comparison_interface/static/images/
+cp -TR $WORKING_DIR/comparison_interface/example.pages_html/ $WORKING_DIR/comparison_interface/pages_html/
 cp $WORKING_DIR/comparison_interface/configuration/example.flask.py $WORKING_DIR/comparison_interface/configuration/flask.py
 cp $WORKING_DIR/.env-example $WORKING_DIR/.env
