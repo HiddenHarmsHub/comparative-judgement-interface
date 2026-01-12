7da2ccf51ecff31def8d260aa19acd798970e5b0
Cat Smith <910384+catsmith@users.noreply.github.com>
2026-01-12 11:36:06 +0000

    allow app to run on subdomains if nginx/uwsgi is setup for that

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
 from comparison_interface import create_app
 
-app = create_app()
+load_dotenv()
+
+subdomain = os.getenv("SUBDOMAIN", "")
+
+if subdomain == "":
+    app = create_app()
+else:
+    app = DispatcherMiddleware(None, {
+        subdomain: create_app()
+    })
