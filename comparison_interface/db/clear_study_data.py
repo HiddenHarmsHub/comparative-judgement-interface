219f8b7fbdf55cf2fe8ebbe1b64fde6c130e2054
Cat Smith <910384+catsmith@users.noreply.github.com>
2026-07-21 16:09:48 +0100

    add a purge button and the purge functionality

---
+++ comparison_interface/db/clear_study_data.py
@@ -0,0 +1,18 @@
+from sqlalchemy import delete, update
+
+from .connection import db
+from .models import Comparison, Participant, ParticipantGroup, ParticipantItem, TotalItemPair
+
+
+def clear_study_data():
+    """Clear the participant and judgement data from the database and reset the TotalItemPair judgement tracker."""
+    tables_to_clear = [Comparison, ParticipantGroup, ParticipantItem, Participant]
+
+    db_engine = db.engines['study_db']
+    with db_engine.begin() as connection:
+        for table in tables_to_clear:
+            connection.execute(delete(table))
+
+        connection.execute(
+            update(TotalItemPair).values(judged=False)
+        )
